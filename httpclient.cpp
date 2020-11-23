5f1ae29312e8adb26af598b705a559c38639fc2e
Lee <goldholder@outlook.com>
2020-11-24 00:22:43 +0900

    초기 커밋.

---
+++ httpclient.cpp
@@ -0,0 +1,76 @@
+
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <cpprest/uri.h>
+#include <cpprest/asyncrt_utils.h>
+
+#include <iostream>
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+using namespace std;
+
+void GetHttp()
+{
+    http_client client(U("http://en.cppreference.com/w/"));
+    auto resp = client.request(U("GET")).get();
+
+    wcout << U("STATUS : ") << resp.status_code() << endl;
+    wcout << "content-type : " << resp.headers().content_type() << endl;
+    wcout << resp.extract_string(true).get() << endl;
+}
+
+void GetHttpAsync()
+{
+    http_client client(U("http://en.cppreference.com/w/"));
+
+    client.request(U("GET")).then([](http_response resp) {
+        wcout << U("STATUS : ") << resp.status_code() << endl;
+        wcout << "content-type : " << resp.headers().content_type() << endl;
+
+        resp.extract_string(true).then([](string_t sBoby) {
+            wcout << sBoby << endl;
+            }).wait();
+
+        }).wait();
+
+}
+
+
+void GetJson()
+{
+    http_client client(U("http://date.jsontest.com/"));
+
+    http_request req(methods::GET);
+
+    client.request(req).then([=](http_response r) {
+        wcout << U("STATUS : ") << r.status_code() << endl;
+        wcout << "content-type : " << r.headers().content_type() << endl;
+
+        //{
+        //		"time": "11:25:23 AM",
+        //		"milliseconds_since_epoch" : 1423999523092,
+        //		"date" : "02-15-2015"
+        //}
+
+        r.extract_json(true).then([](json::value v) {
+            wcout << v.at(U("date")).as_string() << endl;
+            wcout << v.at(U("time")).as_string() << endl;
+            }).wait();
+
+        }).wait();
+
+}
+
+int main(int argc, char argv[])
+{
+    GetHttp();
+    GetHttpAsync();
+    GetJson();
+    return 0;
+}
---
+++ httpserver.cpp
@@ -0,0 +1,73 @@
+﻿// httpserver.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <cpprest/uri.h>
+#include <cpprest/asyncrt_utils.h>
+
+#include <iostream>
+
+using namespace web::http::experimental::listener;
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
+using namespace std;
+
+
+
+class CommandHandler
+{
+public:
+    CommandHandler() {}
+    CommandHandler(utility::string_t url);
+    pplx::task<void> open() { return m_listener.open(); }
+    pplx::task<void> close() { return m_listener.close(); }
+private:
+    void handle_get_or_post(http_request message);
+    http_listener m_listener;
+};
+
+CommandHandler::CommandHandler(utility::string_t url) : m_listener(url)
+{
+    m_listener.support(methods::GET, std::bind(&CommandHandler::handle_get_or_post, this, std::placeholders::_1));
+    m_listener.support(methods::POST, std::bind(&CommandHandler::handle_get_or_post, this, std::placeholders::_1));
+}
+
+void CommandHandler::handle_get_or_post(http_request message)
+{
+    ucout << "Method: " << message.method() << std::endl;
+    ucout << "URI: " << http::uri::decode(message.relative_uri().path()) << std::endl;
+    ucout << "Query: " << http::uri::decode(message.relative_uri().query()) << std::endl << std::endl;
+    message.reply(status_codes::OK, "ACCEPTED");
+};
+
+int main(int argc, char argv[])
+{
+    try
+    {
+        utility::string_t address = U("http://127.0.0.1:9999/api");
+        uri_builder uri(address);
+        auto addr = uri.to_uri().to_string();
+        CommandHandler handler(addr);
+        handler.open().wait();
+       
+        ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
+        ucout << U("Press ENTER key to quit...") << std::endl;
+        std::string line;
+        std::getline(std::cin, line);
+        handler.close().wait();
+
+    }
+    catch (std::exception& ex)
+    {
+        ucout << U("Exception: ") << ex.what() << std::endl;
+        ucout << U("Press ENTER key to quit...") << std::endl;
+        std::string line;
+        std::getline(std::cin, line);
+    }
+    return 0;
+}
